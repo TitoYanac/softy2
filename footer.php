405f7373013f67fc2fd453fff20310a14c85081d
titoyanac <titoyanac@gmail.com>
2020-01-03 04:43:15 -0500

    primer avance

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
 <footer>
 	<p>Todos los derechos reservados 2020</p>
 	<script type="text/javascript" src="js/js.js"></script>
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </footer>
 </body>
 </html>
--- header.php
+++ header.php
@@ -4,41 +4,40 @@
 		<title>Softy Company</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="icon" type="img/png" href="img/softy-logo.png">
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="icon" type="img/svg" href="img/logo_softy.svg">
 		<link rel="stylesheet" href="css/estilos.css">
 	</head>
 <body>
 	<article id="loadScreen">
-		<figure class="figure">
-			<img src="img/logo_softy_f_claro.svg" class="figure-img img-fluid rounded" alt="Logo.png">
-			<figcaption id="loading" class="figure-caption"></figcaption>
+		<figure>
+			<img src="img/logo_softy_f_claro.svg" alt="logo_softy_f_claro.svg">
+			<figcaption id="loading"></figcaption>
 			<p>cargando...</p>
 		</figure>
 	</article>
 	<header>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand" href="index.php">
-				<img src="img/logo_softy_f_oscuro.svg" class="figure-img img-fluid rounded logo" alt="softy-logo-40px.png">
+		<nav>
+			<a href="index.php">
+				<img src="img/logo_softy_f_oscuro.svg" alt="logo_softy_f_oscuro.svg">
 			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
+			<button id="menu-mobile-btn">
+				<span class="btn-icon"></span>
 			</button>
 
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item active"><a class="nav-link" href="index.php">Inicio<span class="sr-only">(current)</span></a></li>
-					<li class="nav-item"       ><a class="nav-link" href="nosotros.php">Nosotros</a></li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Servicios</a>
-						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="web.php">Desarrollo Web</a>
-							<a class="dropdown-item" href="mobile.php">Desarrollo Mobile</a>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item" href="servicios.php">Marketing Digital </a>
+			<div>
+				<ul id="nav-header">
+					<li><a class="list-item"   href="index.php">Inicio</a></li>
+					<li><a class="list-item"   href="nosotros.php">Nosotros</a></li>
+					<li id="dropdown">
+						<a class="list-item"   href="#">Servicios</a>
+						<div id="dropdown-header">
+							<a class="list-item"  href="web.php">Desarrollo Web</a>
+							<a class="list-item"  href="mobile.php">Desarrollo Mobile</a>
+							<div id="dropdown-divider"></div>
+							<a class="list-item"  href="marketing.php">Marketing Digital</a>
 						</div>
 					</li>
-					<li class="nav-item"       ><a class="nav-link btn btn-warning btn-cotiza" href="#">Cotiza tu proyecto Aquí!</a></li>
+					<li><a id="cotiza-btn" href="#">Cotiza tu proyecto Aquí!</a></li>
 				</ul>
 			</div>
 		</nav>
